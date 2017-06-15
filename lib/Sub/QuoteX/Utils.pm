183e911a3925893313469244272c4ed1a6f5baa7
Diab Jerius <djerius@cfa.harvard.edu>
2017-06-15 15:22:47 -0400

    bump version

--- lib/Sub/QuoteX/Utils.pm
+++ lib/Sub/QuoteX/Utils.pm
@@ -7,7 +7,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 use Sub::Quote
   qw( quoted_from_sub inlinify capture_unroll sanitize_identifier quote_sub );
